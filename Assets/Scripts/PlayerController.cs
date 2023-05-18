95d6400afb1c132fdb69aa24c524fc7c301118d1
Jon <alsomjon@hotmail.com>
2023-05-18 14:34:48 -0400

    Updated movement Script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -19,15 +19,79 @@ public class PlayerController : MonoBehaviour
     public float maxSpeed;
     public float acceleration;
     public float movementSpeed;
-    void Update()
-    {
+    public float runModifier;
+    public float crouchModifier;
+
+    [Header("Slope Handler")]
+    public float slopeModifier;
+    public enum MovementState {
+        Crouching,
+        Walking,
+        Running,
+    }
+    public MovementState movementState = MovementState.Walking;
+    public enum AerialState {
+        Grounded,
+        Air
+    }
+    
+    public AerialState aerialState = AerialState.Air;
+    
+    
+    void Update() {
         CameraHandler();
         MovementHandler();
+        CalculateSlope();
     }
 
+
+
+    void Crouch() {
+        if (movementState != MovementState.Crouching){
+            // Put the player lower so he is properly on the ground when crouching
+            MeshRenderer renderer = playerObj.GetComponent<MeshRenderer>();
+            float height = renderer.bounds.size.y;
+            height /= 4;
+            Vector3 pos = transform.position;
+            pos.y -= height;
+            transform.position = pos;
+
+            // Scales the player down so his hitbox is smaller and consistent with a crouched person
+            Vector3 scale = transform.localScale;
+            scale.y /= 2;
+            transform.localScale = scale;
+            
+        }
+    }
+    void UnCrouch() {
+        if (movementState == MovementState.Crouching){
+            // Scales the player back up to normal size
+            Vector3 scale = transform.localScale;
+            scale.y *= 2;
+            transform.localScale = scale;
+
+            // Puts the player back to normal position, stopping them from spamming crouch to go down.
+            MeshRenderer renderer = playerObj.GetComponent<MeshRenderer>();
+            float height = renderer.bounds.size.y;
+            height /= 4;
+            Vector3 pos = transform.position;
+            pos.y += height;
+            transform.position = pos;
+        }
+    }
+
+    (float, float) CalculateSlope() {
+
+        RaycastHit rchit;
+        Physics.Raycast(transform.position, -Vector3.up, out rchit);
+        float slopeAngle = Vector3.Angle(Vector3.up, rchit.normal);
+        float slopeDirection = Mathf.Atan2(rchit.normal.x, rchit.normal.z) * Mathf.Rad2Deg;
+        return (slopeAngle, slopeDirection);
+    }
     (float, float) MovementInputHandler() {
         float v = 0;
         float h = 0;
+        // Basic WASD movement
         if (Input.GetKey(KeyCode.W)){
             v = 1;
         }
@@ -40,30 +104,45 @@ public class PlayerController : MonoBehaviour
         if (Input.GetKey(KeyCode.D)) {
             h = 1;
         }
+        // If we move diagonally, slow the player down
         if (Mathf.Abs(h) + Mathf.Abs(v) > 1) {
             h *= 0.5F;
             v *= 0.5F;
         }
+        // Handles setting movementStates and changing velocities based on those.
+        if (Input.GetKey(KeyCode.LeftControl)) {
+            Crouch();
+            movementState = MovementState.Crouching;
+            h *= crouchModifier;
+            v *= crouchModifier;
+        } else {
+            UnCrouch();
+            if (Input.GetKey(KeyCode.LeftShift)) {
+                movementState = MovementState.Running;
+                if (v > 0) {
+                    v *= runModifier;
+                }
+            } else {
+                movementState = MovementState.Walking;
+            }
+        }
+        (float slope, float slopeDirection) = CalculateSlope();
+        // Handles changing velocity on a slope
         return (h, v);
     }
     void MovementHandler() {
 
 
         Rigidbody rb = GetComponent<Rigidbody>();
+        rb.drag = movementSpeed;
         var locVel = transform.InverseTransformDirection(rb.velocity);
         (float h, float v) = MovementInputHandler();
         float x = locVel.x;
         float z = locVel.z;
 
 
-        if (Mathf.Abs(locVel.x) < movementSpeed) {
-            x += h * acceleration;
-        } 
-        
-        if (Mathf.Abs(locVel.z) < movementSpeed) {
-            z += v * acceleration;
-        } 
-
+        rb.AddForce(transform.forward * (v * acceleration), ForceMode.Impulse);
+        rb.AddForce(transform.right * (h * acceleration), ForceMode.Impulse);
         locVel = new Vector3(x, locVel.y, z);
         rb.velocity = transform.TransformDirection(locVel);
 
